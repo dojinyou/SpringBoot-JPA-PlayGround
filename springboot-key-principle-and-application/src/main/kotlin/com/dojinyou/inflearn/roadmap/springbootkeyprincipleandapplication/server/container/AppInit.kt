39c6e780e166aba048690ba021c7f1c0b9a3dc49
dojinyou <djyou128@gmail.com>
2023-03-25 19:53:12 +0900

    폴더 정리

--- springboot-key-principle-and-application/src/main/kotlin/com/dojinyou/inflearn/roadmap/springbootkeyprincipleandapplication/server/container/AppInit.kt
+++
@@ -1,7 +0,0 @@
-package com.dojinyou.inflearn.roadmap.springbootkeyprincipleandapplication.server.container
-
-import jakarta.servlet.ServletContext
-
-interface AppInit {
-    fun onStartup(servletContext: ServletContext?)
-}
