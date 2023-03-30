ab82b894db71eaf0a064e5efbda66fff119ce835
dojinyou <djyou128@gmail.com>
2023-03-30 21:40:12 +0900

    [w3]set up

---
+++ w3/src/main/kotlin/com/dojinyou/inflearn/w5/W5Application.kt
@@ -0,0 +1,11 @@
+package com.dojinyou.inflearn.w5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W5Application
+
+fun main(args: Array<String>) {
+	runApplication<W5Application>(*args)
+}
